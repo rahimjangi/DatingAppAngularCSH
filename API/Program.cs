dd9087e985b0ac8172e4873ea52d1f638a215091
pc <rahimjangi1982@gmail.com>
2024-01-20 20:17:41 -0400

    Client Initial commit

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("default"));
+});
 
 
 var app = builder.Build();
